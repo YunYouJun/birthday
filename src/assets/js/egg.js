30e323d652f5750337ddab4da756db604d6e1b91
YunYouJun <me@yunyoujun.cn>
2020-10-17 12:38:59 +0800

    :arrow_up: upgrade dependencies & use gh actions replace travis

--- src/assets/js/egg.js
+++ src/assets/js/egg.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 console.log(
   "%c Happy Birthday! ",
-  "background-color:#93B7F3;color:white;padding:2px;border-radius: 2px;"
+  "background-color:#93B7F3;color:white;padding:2px;"
 );
 console.log(
   "%c 虽然离死亡更进一步，但祝你新的一岁更加开心！",
-  "background-color:dodgerblue;color:white;padding:2px;border-radius: 2px;"
+  "background-color:dodgerblue;color:white;padding:2px;"
 );
 console.log(
-  "%c From your friend. @YunYouJun ",
-  "background-color:#000000;color:white;padding:2px;border-radius: 2px;"
+  "%c From your friend. @YunYouJun %c https://github.com/YunYouJun/birthday ",
+  "background-color:#000000;color:white;padding:3px;",
+  "border: 1px solid black;padding:2px;"
 );
